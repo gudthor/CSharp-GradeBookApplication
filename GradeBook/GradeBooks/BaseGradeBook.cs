da80fb69a06421a3d8e00108ec3ee1f567372be8
gudthor <gud_thor@hotmail.com>
2019-04-09 22:55:09 +0100

    test commit

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,7 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        //Test change beeing pushed to GITHUB
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
